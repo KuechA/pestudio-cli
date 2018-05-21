fcd45967414d888b6a0ed5e8a88297219d1f88df
KuechA <kuechleralexander1@gmail.com>
2018-05-21 11:54:57 +0200

    Matching thhe signature of packers in the PE file

--- SignatureMatcher.py
+++ SignatureMatcher.py
@@ -18,22 +18,35 @@ class SignatureMatcher:
 		self.peFile = pepy.parse(file)
 
 	def getSignatures(self):
+		'''
+		Parses signature.xml file to extract the signature names and patterns
+		'''
 		sigs = ET.parse("xml/signatures.xml").getroot().find('sigs')
 		signatures = []
+		maxSize = 0
 		for sig in sigs.findall('sig'):
 			name = sig.find('text').text
 			signature = sig.find('pattern').text
-			signature = re.sub(r"\s+", " ", signature)
+			signature = re.sub(r"\s+", "", signature)
+			signature = signature.replace("x", ".").lower()
+			if len(signature) > maxSize:
+				maxSize = len(signature)
 			ep = sig.find('ep').text == 'true'
 			signatures.append(Signature(name, signature, ep))
-		return signatures
+		return signatures, maxSize
 
-	def findMatches(self):
+	def findPackers(self, signatures, maxSize):
+		'''
+		Scans the PE file for signatures we use to find packers
+		'''
+		matches = []
 		for sect in self.peFile.get_sections():
 			if sect.length:
-				print("\tSection: %s, First 10 bytes: 0x%s" % (sect.name, binascii.hexlify(sect.data[:10])))
-			else:
-				print("\Section %s has no data" % sect.name)
+				sectStart = str(binascii.hexlify(sect.data[:maxSize]))[2:-1]
+				for signature in signatures:
+					if re.match(signature.sig, sectStart):
+						matches.append(signature.name)
+		return matches
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='PE file analyzer')
@@ -41,5 +54,9 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 	
 	matcher = SignatureMatcher(args.file)
-	print([str(s) for s in matcher.getSignatures()])
-	matcher.findMatches()
+	signatures, maxSize = matcher.getSignatures()
+	packers = matcher.findPackers(signatures, maxSize)
+	if len(packers):
+		print("The signature of the following packer was found: ", packers)
+	else:
+		print("No packer signature was found in the PE file")
