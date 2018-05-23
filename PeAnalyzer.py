a55427f8f825327cc32b4eb33a1707a88f3cc917
KuechA <kuechleralexander1@gmail.com>
2018-05-23 20:54:29 +0200

    Finalized migration to liaf library

--- PeAnalyzer.py
+++ PeAnalyzer.py
@@ -18,8 +18,19 @@ class Import:
 	def __str__(self):
 		return self.lib + ": " + self.fct + ", blacklisted: " + str(self.blacklisted) + " with group: " + str(self.group)
 
+class Resource:
+	def __init__(self, type, name, language, md5):
+		self.type = type
+		self.name = name
+		self.language = language
+		self.md5 = md5
+
+	def __str__(self):
+		return str(self.name) + " of type " + str(self.type) + ", language " + str(self.language) + " has md5 " + str(self.md5)
+
 class PeAnalyzer:
 	imports = None
+	resources = None
 
 	def __init__(self, file):
 		self.peFile = lief.parse(file)
@@ -81,7 +92,7 @@ class PeAnalyzer:
 					function.group = groups[fct.attrib['group']]
 					function.blacklisted = True
 				self.suspiciousFunctions += f
-		
+		# TODO: We can replace the suspicious functions with filtering for imports which are blacklisted
 		return self.suspiciousFunctions, self.imports
 
 	def printImportInformation(self):
@@ -114,13 +125,25 @@ class PeAnalyzer:
 		ET.SubElement(summary, "blacklisted").text = str(len(self.suspiciousFunctions))
 		ET.SubElement(summary, "total").text = str(len(self.imports))
 		blacklisted = ET.SubElement(imports, "blacklisted")
+		# TODO: We can also show all functions and tell which ones are blacklisted
 		for imp in self.suspiciousFunctions:
 			fct = ET.SubElement(blacklisted, "function")
-			fct.set("library", imp['lib'])
-			fct.set("group", imp['group'])
-			fct.text = imp['fct']
+			fct.set("library", imp.lib)
+			fct.set("group", imp.group)
+			fct.set("blacklisted", str(imp.blacklisted))
+			fct.text = imp.fct
 		
 		return root
+
+	def __getResources(self):
+		self.resources = []
+		for resourceType in self.peFile.resources.childs:
+			for resource in resourceType.childs:
+				for lang in resource.childs:
+					name = resource.name if resource.has_name else hex(resource.id)
+					self.resources.append(Resource(resourceType.id, name, lang.id, hashlib.md5(bytes(lang.content))))
+		
+		return self.resources
 
 	def blacklistedResources(self):
 		'''
@@ -128,8 +151,11 @@ class PeAnalyzer:
 		are used by the PE file to analyze.
 		'''
 		# Get the MD5 of resources used by the PE file
-		resourceMD5 = [hashlib.md5(r.data).hexdigest().upper() for r in self.peFile.get_resources()]
+		#resourceMD5 = [hashlib.md5(r.data).hexdigest().upper() for r in self.peFile.resources]
+		if self.resources is None:
+			self.__getResources()
 		
+		resourceMD5 = [res.md5 for res in self.resources]
 		# Get the program name from translations file
 		translations = ET.parse("xml/translations.xml").getroot().find('knownResources')
 		dict = {}
@@ -166,16 +192,13 @@ class PeAnalyzer:
 		languages = self.__get_languages()
 		
 		allResources = ET.SubElement(resources, "resource-list")
-		for resource in self.peFile.get_resources():
-			type = resource.type_as_str()
-			name = resource.name_str if resource.name_str else resource.name
-			md5 = hashlib.md5(resource.data).hexdigest()
-			language = languages[resource.lang]
+		for resource in self.resources:
+			name = resource.name #resource.name_str if resource.name_str else hex(esource.name)
 			res = ET.SubElement(allResources, "resource")
-			res.set("type", type)
-			res.set("name", hex(name))
-			res.set("language", language)
-			res.text = md5
+			res.set("type", resource.type)
+			res.set("name", resource.name)
+			res.set("language", languages[resource.language])
+			res.text = resource.md5.hexdigest().upper()
 		
 		return root
 
@@ -187,12 +210,12 @@ class PeAnalyzer:
 		# and in translations.xml we have a "severity" value
 		table = prettytable.PrettyTable()
 		table.field_names = ["Type", "Name", "MD5", "Language"]
-		for resource in self.peFile.get_resources():
-			type = resource.type_as_str()
-			name = resource.name_str if resource.name_str else hex(resource.name)
-			md5 = hashlib.md5(resource.data).hexdigest()
-			language = languages[resource.lang]
-			table.add_row([type, name, md5, language])
+		for resource in self.resources:
+			type = resource.type
+			name = resource.name
+			md5 = resource.md5
+			language = resource.language
+			table.add_row([type, name, md5.hexdigest().upper(), languages[language]])
 		
 		resultString = str(re.sub(r'(^|\n)', r'\1\t', str(table)))
 		print(resultString)
@@ -200,24 +223,25 @@ class PeAnalyzer:
 	def addHeaderInformationXml(self, root):
 		header = ET.SubElement(root, "FileHeader")
 		signature = ET.SubElement(header, "signature")
-		signature.text = hex(self.peFile.signature)
+		if self.peFile.has_signature:
+			signature.text = hex(self.peFile.signature)
 		machine = ET.SubElement(header, "machine")
-		machine.text = constants.MACHINE_TYPE[self.peFile.machine]
+		machine.text = constants.MACHINE_TYPE[self.peFile.machine_type]
 		sections = ET.SubElement(header, "numberOfSections")
-		sections.text = hex(self.peFile.numberofsections)
+		sections.text = hex(self.peFile.numberof_sections)
 		timeDateStamp = ET.SubElement(header, "numberOfSections")
-		timeDateStamp.text = str(datetime.datetime.fromtimestamp(self.peFile.timedatestamp))
+		timeDateStamp.text = str(datetime.datetime.fromtimestamp(self.peFile.time_date_stamps))
 		pointerToSymbolTable = ET.SubElement(header, "pointerToSymbolTable")
-		pointerToSymbolTable.text = hex(self.peFile.pointertosymboltable)
+		pointerToSymbolTable.text = hex(self.peFile.pointerto_symbol_table)
 		numberOfSymbols = ET.SubElement(header, "numberOfSymbols")
-		numberOfSymbols.text = str(self.peFile.numberofsymbols)
+		numberOfSymbols.text = str(self.peFile.numberof_symbols)
 		sizeOfOptionalHeader = ET.SubElement(header, "sizeOfOptionalHeader")
-		sizeOfOptionalHeader.text = str(self.peFile.sizeofoptionalheader)
+		sizeOfOptionalHeader.text = str(self.peFile.sizeof_optional_header)
 		characteristics = ET.SubElement(header, "characteristics")
 		characteristics.text = hex(self.peFile.characteristics)
 		PE32 = ET.SubElement(header, "PE32")
 		PE32.text = str(self.peFile.magic == 267)
-		
+
 		return root
 
 	def printHeaderInformation(self):
@@ -226,27 +250,26 @@ class PeAnalyzer:
 		table.align["Property"] = "l"
 		table.align["Value"] = "l"
 		
-		signature = self.peFile.signature
-		table.add_row(["Signature", hex(signature)])
-		machine = self.peFile.machine
+		table.add_row(["Signature", "".join(["{0:02x}".format(x) for x in self.peFile.header.signature])])
+		machine = self.peFile.header.machine
 		table.add_row(["Machine", constants.MACHINE_TYPE[machine]])
-		sections = self.peFile.numberofsections
+		sections = self.peFile.header.numberof_sections
 		table.add_row(["Number of sections", sections])
-		timeDateStamp = datetime.datetime.fromtimestamp(self.peFile.timedatestamp)
+		timeDateStamp = datetime.datetime.fromtimestamp(self.peFile.header.time_date_stamps)
 		if timeDateStamp > datetime.datetime.now():
 			# The compile date is in the future
 			table.add_row(["timeDateStamp", constants.RED + str(timeDateStamp) + constants.RESET])
 		else:
 			table.add_row(["timeDateStamp", timeDateStamp])
-		pointerToSymbolTable = self.peFile.pointertosymboltable
+		pointerToSymbolTable = self.peFile.header.pointerto_symbol_table
 		table.add_row(["pointerToSymbolTable", hex(pointerToSymbolTable)])
-		numberOfSymbols = self.peFile.numberofsymbols
+		numberOfSymbols = self.peFile.header.numberof_symbols
 		table.add_row(["numberOfSymbols", numberOfSymbols])
-		sizeOfOptionalHeader = self.peFile.sizeofoptionalheader
+		sizeOfOptionalHeader = self.peFile.header.sizeof_optional_header
 		table.add_row(["sizeOfOptionalHeader", sizeOfOptionalHeader])
-		characteristics = self.peFile.characteristics
-		table.add_row(["characteristics", characteristics])
-		PE32 = (self.peFile.magic == 267)
+		characteristics = self.peFile.header.characteristics
+		table.add_row(["characteristics", hex(characteristics)])
+		PE32 = (self.peFile.dos_header.magic == 267)
 		table.add_row(["Processor 32-bit", PE32])
 		if timeDateStamp > datetime.datetime.now():
 			print("File Header: %sSuspicious value for TimeDateStamp (%s)%s" % (constants.RED, str(timeDateStamp) ,constants.RESET))
