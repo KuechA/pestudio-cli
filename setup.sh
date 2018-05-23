a154f21550e8f34ee3b457a08a6875eccebf5a9a
KuechA <kuechleralexander1@gmail.com>
2018-05-23 20:59:52 +0200

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,3 @@
 pip3 install requests, prettytable
-
-# Install pe-parse and python library pepy
-git clone https://github.com/KuechA/pe-parse.git
-cd pe-parse
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-cmake --build . --config Release
-cmake --build . --config Release --target install
-cd ../python
-python setup.py build
-python setup.py install
-
+pip3 install setuptools --upgrade
+pip3 install lief
