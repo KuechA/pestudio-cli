007079248898d5614cd8ed3fa6dbdd68006042b8
Giovi0298 <giovi.loc98@gmail.com>
2022-11-12 13:18:39 +0100

    Change the path definition, attribute checker and variable name
    - Added folder path to read xml files (needed when the tool is used outside its folder)
    - Modifyed the evaluation of the attributes for the lack of presence check (due to crash)
    - Modifyed some variable names for collisions or missing definitions

--- SignatureMatcher.py
+++ SignatureMatcher.py
@@ -2,74 +2,81 @@ import lief
 import argparse
 import xml.etree.ElementTree as ET
 import re
+import os
+path, filename = os.path.split(__file__)
+path += "/"
+
 
 class Signature:
-	def __init__(self, name, sig, ep):
-		self.name = name
-		self.sig = sig
-		self.ep = ep
+    def __init__(self, name, sig, ep):
+        self.name = name
+        self.sig = sig
+        self.ep = ep
 
-	def __str__(self):
-		return "\"" + self.name + "\" having signature " + self.sig + ", ep: " + str(self.ep)
+    def __str__(self):
+        return "\"" + self.name + "\" having signature " + self.sig + ", ep: " + str(self.ep)
+
 
 class SignatureMatcher:
-	def __init__(self, file):
-		self.peFile = lief.parse(file)
+    def __init__(self, file):
+        self.peFile = lief.parse(file)
 
-	def getSignatures(self):
-		'''
-		Parses signature.xml file to extract the signature names and patterns
-		'''
-		sigs = ET.parse("xml/signatures.xml").getroot().find('sigs')
-		self.signatures = []
-		self.maxSize = 0
-		for sig in sigs.findall('sig'):
-			name = sig.find('text').text
-			signature = sig.find('pattern').text
-			signature = re.sub(r"\s+", "", signature)
-			signature = signature.replace("x", ".").lower()
-			if len(signature) > self.maxSize:
-				self.maxSize = len(signature)
-			ep = sig.find('ep').text == 'true'
-			self.signatures.append(Signature(name, signature, ep))
-		return self.signatures, self.maxSize
+    def getSignatures(self):
+        '''
+        Parses signature.xml file to extract the signature names and patterns
+        '''
+        sigs = ET.parse(path+"xml/signatures.xml").getroot().find('sigs')
+        self.signatures = []
+        self.maxSize = 0
+        for sig in sigs.findall('sig'):
+            name = sig.find('text').text
+            signature = sig.find('pattern').text
+            signature = re.sub(r"\s+", "", signature)
+            signature = signature.replace("x", ".").lower()
+            if len(signature) > self.maxSize:
+                self.maxSize = len(signature)
+            ep = sig.find('ep').text == 'true'
+            self.signatures.append(Signature(name, signature, ep))
+        return self.signatures, self.maxSize
 
-	def findPackers(self):
-		'''
-		Scans the PE file for signatures we use to find packers
-		'''
-		self.getSignatures()
-		
-		self.matches = []
-		for sect in self.peFile.sections:
-			if sect.size:
-				sectStart = "".join(["{0:02x}".format(x) for x in sect.content[:self.maxSize]])
-				for signature in self.signatures:
-					if re.match(signature.sig, sectStart):
-						self.matches.append(signature.name)
-		return self.matches
-	
-	def addPackersXml(self, root):
-		packers = ET.SubElement(root, "Packers")
-		for match in self.matches:
-			packer = ET.SubElement(packers, "packer")
-			packer.text = match
-		
-		return root
-	
-	def addPackersJson(self, jsonDict):
-		jsonDict["Packers"] = self.matches
-		return jsonDict
+    def findPackers(self):
+        '''
+        Scans the PE file for signatures we use to find packers
+        '''
+        self.getSignatures()
+
+        self.matches = []
+        for sect in self.peFile.sections:
+            if sect.size:
+                sectStart = "".join(["{0:02x}".format(x)
+                                    for x in sect.content[:self.maxSize]])
+                for signature in self.signatures:
+                    if re.match(signature.sig, sectStart):
+                        self.matches.append(signature.name)
+        return self.matches
+
+    def addPackersXml(self, root):
+        packers = ET.SubElement(root, "Packers")
+        for match in self.matches:
+            packer = ET.SubElement(packers, "packer")
+            packer.text = match
+
+        return root
+
+    def addPackersJson(self, jsonDict):
+        jsonDict["Packers"] = self.matches
+        return jsonDict
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='PE file analyzer')
-	parser.add_argument("-f", "--file", help="The file to analyze", required=True, dest="file")
-	args = parser.parse_args()
-	
-	matcher = SignatureMatcher(args.file)
-	packers = matcher.findPackers()
-	if len(packers):
-		print("The signature of the following packer was found: ", packers)
-	else:
-		print("No packer signature was found in the PE file")
+    parser = argparse.ArgumentParser(description='PE file analyzer')
+    parser.add_argument(
+        "-f", "--file", help="The file to analyze", required=True, dest="file")
+    args = parser.parse_args()
+
+    matcher = SignatureMatcher(args.file)
+    packers = matcher.findPackers()
+    if len(packers):
+        print("The signature of the following packer was found: ", packers)
+    else:
+        print("No packer signature was found in the PE file")
--- VirusTotalClient.py
+++ VirusTotalClient.py
@@ -7,121 +7,135 @@ import prettytable
 import re
 import xml.etree.ElementTree as ET
 
-FORMAT = '%(asctime)-15s %(message)s' 
+FORMAT = '%(asctime)-15s %(message)s'
 logging.basicConfig(format=FORMAT)
 logger = logging.getLogger("VirusTotalClient")
 logger.setLevel(50)
 
+
 class VirusTotalClient:
-	
-	report = None
-	
-	def __init__(self, file):
-		self.file = file
-		if not os.path.isfile('VirusTotalApiKey'):
-			self.key = None
-			return
-		
-		with open('VirusTotalApiKey', 'r') as keyFile:
-			self.key = keyFile.read().strip()
 
-	def sendRequest(self):
-		if self.key is None:
-			return None
-		params = {'apikey': self.key}
-		files = {'file': (self.file, open(self.file, 'rb'))}
-		response = requests.post('https://www.virustotal.com/vtapi/v2/file/scan', files=files, params=params)
-		
-		if response.status_code == 200: # Response: OK
-			json_response = response.json()
-			logger.info("Sent: %s, Response: %s", os.path.basename(self.file), str(json_response))
-			self.resourceId = json_response['resource']
-			return self.resourceId
-		else:
-			logger.warning("Sent: %s, HTTP: %d", os.path.basename(self.file), res.status_code)
-			return None
+    report = None
 
-	def getReport(self, resourceId = None):
-		if self.key is None:
-			return None
-		if resourceId is None:
-			resourceId = self.sendRequest()
-		if resourceId is None:
-			return None
-		
-		params = {'apikey': self.key, 'resource': resourceId}
-		headers = {"Accept-Encoding": "gzip, deflate", "User-Agent" : "VirusTotalClient"}
-		response = requests.get('https://www.virustotal.com/vtapi/v2/file/report', params=params, headers=headers)
-		if response.status_code == 200: # Response: OK
-			self.report = response.json()
-			logger.info("Report: %s", str(self.report))
-			return self.report
-		else:
-			logger.warning("Sent: %s, HTTP: %d", os.path.basename(self.file), res.status_code)
-			return None
+    def __init__(self, file):
+        self.file = file
+        if not os.path.isfile('VirusTotalApiKey'):
+            self.key = None
+            return
 
-	def printReport(self, showPositiveResultsOnly = True):
-		self.getReport()
-		if self.report is None:
-			return "Error while getting the result of Virus Total"
-		
-		resultString = "VirusTotal result: " + str(self.report['positives']) + " of " + str(self.report['total']) + " tests are positive\n"
-		if showPositiveResultsOnly and not self.report['positives'] == 0:
-			table = prettytable.PrettyTable()
-			table.field_names = ["Engine", "Version", "Result"]
-			for test, result in self.report["scans"].items():
-				if showPositiveResultsOnly:
-					if result['detected']:
-						table.add_row([test, result['version'], result['result']])
-				else:
-					table.add_row([test, result['version'], result['result']])
-		
-			resultString += str(re.sub(r'(^|\n)', r'\1\t', str(table)))
-		return resultString
-	
-	def getXmlReport(self, root):
-		self.getReport()
+        with open('VirusTotalApiKey', 'r') as keyFile:
+            self.key = keyFile.read().strip()
 
-		vtRes = ET.SubElement(root, "VirusTotal")
-		summary = ET.SubElement(vtRes, "summary")
-		if self.report is None:
-			summary.text = "Error while getting the result of Virus Total"
-			return root
-		
-		positives = ET.SubElement(summary, "positives")
-		positives.text = str(self.report['positives'])
-		total = ET.SubElement(summary, "total")
-		total.text = str(self.report['total'])
-		
-		# Show all the results from virus total
-		details = ET.SubElement(vtRes, "details")
-		for test, result in self.report["scans"].items():
-			res = ET.SubElement(details, "test-result")
-			res.set("engine", str(test))
-			res.set("version", str(result['version']))
-			res.text = str(result['result'])
-		
-		return root
-	
-	def getJsonReport(self, jsonDict):
-		self.getReport()
-		if self.report is None:
-			jsonDict["VirusTotal"] = "Error while getting the result of Virus Total"
-			return jsonDict
-			
-		result = {"summary": {"positives": str(self.report['positives']), "total": str(self.report['total'])}}
-		result["details"] = []
-		for test, result in self.report["scans"].items():
-			result["details"].append({"engine": str(test), "version": str(result['version']), "result": str(result['result'])})
-		
-		jsonDict["VirusTotal"] = result
-		return jsonDict
+    def sendRequest(self):
+        if self.key is None:
+            return None
+        params = {'apikey': self.key}
+        files = {'file': (self.file, open(self.file, 'rb'))}
+        response = requests.post(
+            'https://www.virustotal.com/vtapi/v2/file/scan', files=files, params=params)
+
+        if response.status_code == 200:  # Response: OK
+            json_response = response.json()
+            logger.info("Sent: %s, Response: %s", os.path.basename(
+                self.file), str(json_response))
+            self.resourceId = json_response['resource']
+            return self.resourceId
+        else:
+            logger.warning("Sent: %s, HTTP: %d", os.path.basename(
+                self.file), response.status_code)
+            return None
+
+    def getReport(self, resourceId=None):
+        if self.key is None:
+            return None
+        if resourceId is None:
+            resourceId = self.sendRequest()
+        if resourceId is None:
+            return None
+
+        params = {'apikey': self.key, 'resource': resourceId}
+        headers = {"Accept-Encoding": "gzip, deflate",
+                   "User-Agent": "VirusTotalClient"}
+        response = requests.get(
+            'https://www.virustotal.com/vtapi/v2/file/report', params=params, headers=headers)
+        if response.status_code == 200:  # Response: OK
+            self.report = response.json()
+            logger.info("Report: %s", str(self.report))
+            return self.report
+        else:
+            logger.warning("Sent: %s, HTTP: %d", os.path.basename(
+                self.file), response.status_code)
+            return None
+
+    def printReport(self, showPositiveResultsOnly=True):
+        self.getReport()
+        if self.report is None:
+            return "Error while getting the result of Virus Total"
+
+        resultString = "VirusTotal result: " + \
+            str(self.report['positives']) + " of " + \
+            str(self.report['total']) + " tests are positive\n"
+        if showPositiveResultsOnly and not self.report['positives'] == 0:
+            table = prettytable.PrettyTable()
+            table.field_names = ["Engine", "Version", "Result"]
+            for test, result in self.report["scans"].items():
+                if showPositiveResultsOnly:
+                    if result['detected']:
+                        table.add_row(
+                            [test, result['version'], result['result']])
+                else:
+                    table.add_row([test, result['version'], result['result']])
+
+            resultString += str(re.sub(r'(^|\n)', r'\1\t', str(table)))
+        return resultString
+
+    def getXmlReport(self, root):
+        self.getReport()
+
+        vtRes = ET.SubElement(root, "VirusTotal")
+        summary = ET.SubElement(vtRes, "summary")
+        if self.report is None:
+            summary.text = "Error while getting the result of Virus Total"
+            return root
+
+        positives = ET.SubElement(summary, "positives")
+        positives.text = str(self.report['positives'])
+        total = ET.SubElement(summary, "total")
+        total.text = str(self.report['total'])
+
+        # Show all the results from virus total
+        details = ET.SubElement(vtRes, "details")
+        for test, result in self.report["scans"].items():
+            res = ET.SubElement(details, "test-result")
+            res.set("engine", str(test))
+            res.set("version", str(result['version']))
+            res.text = str(result['result'])
+
+        return root
+
+    def getJsonReport(self, jsonDict):
+        self.getReport()
+        if self.report is None:
+            jsonDict["VirusTotal"] = "Error while getting the result of Virus Total"
+            return jsonDict
+
+        VirusTotal_result = {"summary": {"positives": str(
+            self.report['positives']), "total": str(self.report['total'])}}
+        VirusTotal_result["details"] = []
+        for test, result in self.report["scans"].items():
+            VirusTotal_result["details"].append({"engine": str(test), "version": str(
+                result['version']), "result": str(result['result'])})
+
+        jsonDict["VirusTotal"] = VirusTotal_result
+        return jsonDict
+
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='Virustotal File Scan')
-	parser.add_argument("-f", "--file", help="The file to scan", required=True, dest="file")
-	args = parser.parse_args()
-	
-	vt = VirusTotalClient(args.file)
-	print(vt.printReport(report))
-		
+    parser = argparse.ArgumentParser(description='Virustotal File Scan')
+    parser.add_argument(
+        "-f", "--file", help="The file to scan", required=True, dest="file")
+    args = parser.parse_args()
+
+    vt = VirusTotalClient(args.file)
+    # print(vt.printReport(report))
+    print(vt.printReport())
